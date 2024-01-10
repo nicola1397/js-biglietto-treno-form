0942e7185ed6cca938345aabcab41cc3d4d8e055
nicola1397 <nicola1397@outlook.it>
2024-01-10 17:33:55 +0100

    feat: Added bonus feature and ticket data

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let discounted;
 let sconto;
 let distance;
 let anni;
+let showTicket = document.getElementById("ticket");
 
 document.getElementById("send").addEventListener("click", function () {
   distance = parseInt(document.querySelector("#distance").value);
@@ -34,23 +35,30 @@ document.getElementById("send").addEventListener("click", function () {
   else {
     price = parseFloat((distance * 0.21).toFixed(2));
     console.log(price, "Prezzo");
-    sconto = "Nessuno";
+    sconto = "Nessuna";
+    console.log(sconto);
     discounted = price;
     console.log(discounted, "Scontato");
   }
 });
 
 // DISPLAY DEI VALORI IN HTML
+document.getElementById("send").addEventListener("click", function () {
+  //  VISUALIZZAZIONE BIGLIETTO
+  showTicket.classList.remove("d-none");
 
-// LETTURA NOME
-const nomeCognome = document.getElementById("inputName").value;
-document.getElementById("nome").innerText = nomeCognome;
-// SCONTO
-document.getElementById("offerta").value = sconto;
-// CARROZZA
-document.getElementById("cart").innerText = Math.floor(Math.random() * 15) + 1;
-// CODICE
-document.getElementById("code").innerText =
-  Math.floor(Math.random() * 99999) + 1;
-// PREZZO
-document.getElementById("priceNew").innerText = discounted;
+  // LETTURA NOME
+  const nomeCognome = document.getElementById("inputName").value;
+  console.log(nomeCognome);
+  document.getElementById("nome").innerHTML = nomeCognome;
+  // SCONTO
+  document.getElementById("offerta").innerHTML = sconto;
+  // CARROZZA
+  document.getElementById("cart").innerText =
+    Math.floor(Math.random() * 15) + 1;
+  // CODICE
+  document.getElementById("code").innerText =
+    Math.floor(Math.random() * 99999) + 1;
+  // PREZZO
+  document.getElementById("priceNew").innerHTML = discounted;
+});
